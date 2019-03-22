274898b0aae708af8e974c4d451795fc1d1f46b5
likangzhu <billzhu2008@gmil.com>
2019-03-22 00:35:09 -0500

    initial version

---
+++ teco-cloud/src/main/java/tacos/TecoCloudApplication.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecoCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecoCloudApplication.class, args);
+	}
+
+}
